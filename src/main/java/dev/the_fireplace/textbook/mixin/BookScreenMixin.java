4cd5db77af2fca1252e0bc89fb792209e3c62e6c
The_Fireplace <the.f1repl4ce@gmail.com>
2021-07-07 21:05:33 -0500

    Added checkbox to preserve whitespace when exporting a book. Closes #3

--- src/main/java/dev/the_fireplace/textbook/mixin/BookScreenMixin.java
+++ src/main/java/dev/the_fireplace/textbook/mixin/BookScreenMixin.java
@@ -4,6 +4,7 @@ import dev.the_fireplace.textbook.Textbook;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.BookScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.CheckboxWidget;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -23,10 +24,16 @@ public abstract class BookScreenMixin extends Screen {
 
 	@Inject(at = @At(value="TAIL"), method = "init")
 	private void init(CallbackInfo info) {
+		CheckboxWidget preserveWhitespaceCheckbox = new CheckboxWidget(this.width / 2 + 2, 196 + 40 + 4, 98, 20, new TranslatableText("gui.textbook.export.preserve_whitespace"), true);
+		this.addDrawableChild(preserveWhitespaceCheckbox);
 		this.addButton(new ButtonWidget(this.width / 2 + 2, 196 + 20 + 2, 98, 20, Textbook.getTranslator().getTranslatedString("gui.textbook.export"), (buttonWidget) -> {
 			File exportFile = Textbook.getLogic().fileSaveSelectionDialog();
 			if (exportFile != null) {
-				Textbook.getLogic().exportContents(exportFile, contents);
+				if (preserveWhitespaceCheckbox.isChecked()) {
+					Textbook.getLogic().exportContentsPreservingWhitespace(exportFile, contents);
+				} else {
+					Textbook.getLogic().exportContents(exportFile, contents);
+				}
 			}
 		}));
 	}
