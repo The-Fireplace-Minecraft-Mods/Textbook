bbb7d840af67ad7eec3e9aaccb012ff312591ad6
The_Fireplace <the.f1repl4ce@gmail.com>
2021-07-07 21:05:33 -0500

    Added checkbox to preserve whitespace when exporting a book. Closes #3

--- src/main/java/dev/the_fireplace/textbook/TextbookLogic.java
+++ src/main/java/dev/the_fireplace/textbook/TextbookLogic.java
@@ -52,8 +52,27 @@ public final class TextbookLogic {
             //noinspection HardcodedLineSeparator
             output.append(NEWLINE_REGEX.matcher(contents.getPage(pageIndex).getString()).replaceAll("\r\n")).append(" ");
         }
+        writeStringToFile(file, output.toString());
+    }
+
+    @SuppressWarnings("HardcodedLineSeparator")
+    public void exportContentsPreservingWhitespace(File file, BookScreen.Contents contents) {
+        StringBuilder output = new StringBuilder();
+        for (int pageIndex = 0; pageIndex < contents.getPageCount(); pageIndex++) {
+            StringBuilder pageContents = new StringBuilder(NEWLINE_REGEX.matcher(contents.getPage(pageIndex).getString()).replaceAll("\r\n"));
+            StringBuilder pageWithPreservedWhitespace = new StringBuilder(pageContents);
+            while (fitsOnPage(pageWithPreservedWhitespace.toString())) {
+                pageContents = new StringBuilder(pageWithPreservedWhitespace);
+                pageWithPreservedWhitespace.append("\r\n");
+            }
+            output.append(pageContents);
+        }
+        writeStringToFile(file, output.toString());
+    }
+
+    private void writeStringToFile(File file, String str) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
-            writer.write(output.toString());
+            writer.write(str);
         } catch (IOException e) {
             Textbook.getLogger().error("Unable to export to file!", e);
         }
