4e22b495b24ffc0e2cd37823e48eaf8bdd5c4d48
The_Fireplace <git@the-fireplace.dev>
2025-06-11 22:26:17 -0500

    [1.20.4] Replace old Forge NetworkConstants reference

--- forge/src/main/java/dev/the_fireplace/textbook/entrypoints/Forge.java
+++ forge/src/main/java/dev/the_fireplace/textbook/entrypoints/Forge.java
@@ -9,7 +9,6 @@ import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.network.NetworkConstants;
 
 @Mod("textbook")
 public final class Forge
@@ -22,6 +21,6 @@ public final class Forge
         });
 
         // Register as optional on both sides
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> IExtensionPoint.DisplayTest.IGNORESERVERONLY, (a, b) -> true));
     }
 }
